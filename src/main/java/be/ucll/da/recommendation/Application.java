90c3240ce2b0be98d4ddb9aca17f0cfe4294cafa
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-12-26 14:31:46 +0100

    enabled consul related stuff

--- src/main/java/be/ucll/da/recommendation/Application.java
+++ src/main/java/be/ucll/da/recommendation/Application.java
@@ -2,10 +2,10 @@ package be.ucll.da.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
